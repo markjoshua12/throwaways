07a24f49e3324f4b995c95bf2731ad8635ee3537
markjoshua12 <mark.joshua121001@gmail.com>
2020-09-26 10:49:41 +0800

    Added delay in Shark target finding

--- entity/Shark.py
+++ entity/Shark.py
@@ -9,19 +9,30 @@ class Shark(Mob):
         super().__init__(x, y)
 
         self.target = None
+        self.target_delay = 0
+        self.curr_target_delay = self.target_delay
+
+        self.angle_rad = 0
+        self.move_speed = 0.5
 
     def update(self):
 
         if self.target is None:
             self.look_for_target()
-        else:
+        elif self.curr_target_delay <= 0:
             diff_x = self.target.center_x - self.center_x
             diff_y = self.target.center_y - self.center_y
 
-            self.radians = math.atan2(diff_y, diff_x)
+            self.angle_rad = math.atan2(diff_y, diff_x)
 
-            self.change_x = math.cos(self.radians) * self.move_speed
-            self.change_y = math.sin(self.radians) * self.move_speed
+            self.change_x = math.cos(self.angle_rad) * self.move_speed
+            self.change_y = math.sin(self.angle_rad) * self.move_speed
+
+            self.radians = self.angle_rad
+
+            self.curr_target_delay = self.target_delay
+        else:
+            self.curr_target_delay -= 1
 
         super().update()
 

ccfa6711b36c5573a0ae07a055542386f52bd5af
markjoshua12 <mark.joshua121001@gmail.com>
2020-09-26 10:48:54 +0800

    Fixed Mob class collision with ShipPart

--- entity/Mob.py
+++ entity/Mob.py
@@ -1,5 +1,7 @@
 import arcade
 
+from tiles import Tile
+
 from entity.Entity import Entity
 
 class Mob(Entity):
@@ -31,48 +33,63 @@ class Mob(Entity):
 
     def move(self, dx: float, dy: float):
         
-        if dy != 0:
-            self.center_y += dy
+    # if dy != 0:
+        self.center_y += dy
 
-            # collision_list_y = self.level.get_tiles(
-            #     int(self.left // TILE_SIZE),
-            #     int(self.bottom // TILE_SIZE),
-            #     int(self.right // TILE_SIZE),
-            #     int(self.top // TILE_SIZE))
+        # collision_list_y = self.level.get_tiles(
+        #     int(self.left // TILE_SIZE),
+        #     int(self.bottom // TILE_SIZE),
+        #     int(self.right // TILE_SIZE),
+        #     int(self.top // TILE_SIZE))
 
-            collision_list_y = arcade.check_for_collision_with_list(self, self.level.sprite_list)
+        # collision_list_y = arcade.check_for_collision_with_list(self, self.level.sprite_list)
+        collision_list_y = self.level.sprite_list.spatial_hash.get_objects_for_box(self)
 
-            for entity in collision_list_y:
-                if not entity.is_solid:
+        for entity in collision_list_y:
+            if not entity.is_solid:
+                continue
+            if self.intersects(entity):
+                diff_x = self.center_x - entity.center_x - entity.change_x
+                diff_y = self.center_y - entity.center_y - entity.change_y
+
+                if abs(diff_x) > abs(diff_y):
                     continue
-                if self.intersects(entity):
-                    if self.change_y > 0:
-                        self.center_y = entity.center_y - self.height_2 - entity.height_2
-                    elif self.change_y < 0:
-                        self.center_y = entity.center_y + self.height_2 + entity.height_2
-                    self.collided(entity, 0, dy)
 
-        if dx != 0:
+                if diff_y < 0:
+                    self.center_y = entity.center_y - self.height_2 - entity.height_2
+                elif diff_y > 0:
+                    self.center_y = entity.center_y + self.height_2 + entity.height_2
+                self.collided(entity, 0, dy)
 
-            self.center_x += dx
+    # if dx != 0:
 
-            # collision_list_x = self.level.get_tiles(
-            #     int(self.left // TILE_SIZE),
-            #     int(self.bottom // TILE_SIZE),
-            #     int(self.right // TILE_SIZE),
-            #     int(self.top // TILE_SIZE))
+        self.center_x += dx
 
-            collision_list_x = arcade.check_for_collision_with_list(self, self.level.sprite_list)
+        # collision_list_x = self.level.get_tiles(
+        #     int(self.left // TILE_SIZE),
+        #     int(self.bottom // TILE_SIZE),
+        #     int(self.right // TILE_SIZE),
+        #     int(self.top // TILE_SIZE))
 
-            for entity in collision_list_x:
-                if not entity.is_solid:
+        # collision_list_x = arcade.check_for_collision_with_list(self, self.level.sprite_list)
+        collision_list_x = self.level.sprite_list.spatial_hash.get_objects_for_box(self)
+
+        for entity in collision_list_x:
+            if not entity.is_solid:
+                continue
+            if self.intersects(entity):
+                diff_x = self.center_x - entity.center_x - entity.change_x
+                diff_y = self.center_y - entity.center_y - entity.change_y
+
+                if abs(diff_y) > abs(diff_x):
                     continue
-                if self.intersects(entity):
-                    if self.change_x > 0:
-                        self.center_x = entity.center_x - self.width_2 - entity.width_2
-                    elif self.change_x < 0:
-                        self.center_x = entity.center_x + self.width_2 + entity.width_2
-                    self.collided(entity, dx, 0)
+                    
+                if diff_x < 0:
+                    self.center_x = entity.center_x - self.width_2 - entity.width_2
+                elif diff_x > 0:
+                    self.center_x = entity.center_x + self.width_2 + entity.width_2
+                self.collided(entity, dx, 0)
+
     def attack(self):
         pass
 
--- entity/ShipPart.py
+++ entity/ShipPart.py
@@ -3,12 +3,14 @@ import arcade
 
 from graphics import Textures
 
+from entity.Entity import Entity
+
 from tiles import Tile
 
-class ShipPart(arcade.Sprite):
+class ShipPart(Entity):
 
     def __init__(self, ship, x, y, tile):
-        super().__init__()
+        super().__init__(0, 0)
 
         self.texture = Textures.get_texture(tile, 4)
 
@@ -19,6 +21,8 @@ class ShipPart(arcade.Sprite):
 
         self.center_x = self.ship.center_x - self.x * Tile.TILE_SIZE
         self.center_y = self.ship.center_y - self.y * Tile.TILE_SIZE
+
+        self.is_solid = True if tile >= 2 else False
 
     def update(self):
         self.center_x = self.ship.center_x - self.x * Tile.TILE_SIZE
